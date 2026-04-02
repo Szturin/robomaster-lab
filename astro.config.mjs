31dd6b826a8288fa0a952bacb8a8f919124a7c2a
Szturin <2534136157@qq.com>
2026-04-02 17:05:25 +0800

    Initial Astro site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://szturin.github.io',
+  base: '/robomaster-lab',
+});
