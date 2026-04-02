03d149e4209e85c7f7fee2329cfdd843c4c3a6bd
Szturin <2534136157@qq.com>
2026-04-03 01:48:33 +0800

    chore: auto publish site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://szturin.github.io',
   base: '/robomaster-lab',
+  integrations: [sitemap()],
 });
